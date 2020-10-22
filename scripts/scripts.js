acc657b54cccd93f7359b67b3e69a800c672378a
LauraButkute <laura.butkut@gmail.com>
2020-10-22 19:36:17 +0300

    first-commit

---
+++ scripts/scripts.js
@@ -0,0 +1,24 @@
+// click prev/next to slide show
+let slide_index = 17;  
+        displaySlides(slide_index);  
+        function nextSlide(n) {  
+            displaySlides(slide_index += n);  
+        }  
+        function currentSlide(n) {  
+            displaySlides(slide_index = n);  
+        }  
+        function displaySlides(n) {  
+            let slides = document.getElementsByClassName("slide fade");  
+            if (n > slides.length) { slide_index = 1 }  
+            if (n < 1) { slide_index = slides.length }  
+            for (let i = 0; i < slides.length; i++) {  
+                slides[i].style.display = "none";  
+            }  
+            slides[slide_index - 1].style.display = "block";  
+        }
+
+// display: none -> onclick display:block
+
+// function myFunction() {
+//     document.getElementsById("about").style.display = 'block';
+// }
